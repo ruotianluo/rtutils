a560a843c32fbf79514b2f052a50473dca2bf718
ruotianluo <rluo@ttic.edu>
2021-06-05 16:22:00 -0500

    clean up pl_patch

--- rtutils/pl_patch.py
+++ rtutils/pl_patch.py
@@ -8,17 +8,19 @@ if 'rt' in pl.__author__:
 else:
     RTPL = False
 
+
 def assert_RTPL():
     assert RTPL, 'You are not using rt\'s modified pytorch_lightning.\n' \
         'Install pytorch lightning from https://github.com/ruotianluo/pytorch-lightning;\n' \
         'for example, pip install git+https://github.com/ruotianluo/pytorch-lightning or \n' \
         'pip install git+https://github.com/ruotianluo/pytorch-lightning@{tag}'
 
+
 assert_RTPL()
 
-# https://github.com/PyTorchLightning/pytorch-lightning/blob/HEAD/pytorch_lightning/trainer/data_loading.py?q=replace_ddp_sampler#L217
-# use my distributed sampler!
+
 def _get_distributed_sampler(self, dataloader, shuffle, mode):
+    # modified from https://github.com/PyTorchLightning/pytorch-lightning/blob/HEAD/pytorch_lightning/trainer/data_loading.py?q=replace_ddp_sampler#L217
     from pytorch_lightning.utilities import _TORCH_GREATER_EQUAL_1_6
     from pytorch_lightning.overrides.distributed import UnrepeatedDistributedSampler
     from pytorch_lightning.trainer.states import RunningStage
@@ -30,8 +32,11 @@ def _get_distributed_sampler(self, dataloader, shuffle, mode):
     if cls == DeterministicDistributedSampler:
         kwargs["batch_size"] = dataloader.batch_size
     sampler = cls(dataloader.dataset, **kwargs)
-    # If we want to know if it is the end of an epoch, we cannot set_epoch here. because we rely on fixed number of batches each epoch.
-    # sampler.set_epoch(self.total_batch_idx) # we set_epoch here so that the progress bar will know the correct size. In fact pl will set_epoch later and override this. This is just a spoiler for the trainer to know how much batches left for this epoch.
+
+    # rt note here: we set_epoch before, because we want the progress bart start from 0/{remaining batches}, instead of 0/{full batches}. However, we have now a better way to handle progressbar.
+    # I just leave a note here to remind myself
+    # # If we want to know if it is the end of an epoch, we cannot set_epoch here. because we rely on fixed number of batches each epoch.
+    # # sampler.set_epoch(self.total_batch_idx) # we set_epoch here so that the progress bar will know the correct size. In fact pl will set_epoch later and override this. This is just a spoiler for the trainer to know how much batches left for this epoch.
     return sampler
 
 
@@ -42,31 +47,23 @@ def patch_pl_trainer_with_deterministic_sampler(trainer):
     trainer._get_distributed_sampler = types.MethodType(_get_distributed_sampler, trainer)
 
 
-# class ProgressBarPatch(pl.callbacks.Callback):
-
-#     def on_train_epoch_start(self, trainer, pl_module):
-#         # This on_train_epoch_start will run after progress_bar on_train_epoch_start
-#         # this manually set the progress bar to the middle according to the total_batch_idx.
-#         # asssume fix num batches in each epoch.
-#         for callback in trainer.callbacks:
-#             if type(callback) is pl.callbacks.progress.ProgressBar:
-#                 callback.main_progress_bar.update(trainer.total_batch_idx % trainer.num_training_batches)
-
-
 class ProgressBarPatch(pl.callbacks.ProgressBar):
-
     def on_train_epoch_start(self, trainer, pl_module):
         super().on_train_epoch_start(trainer, pl_module)
         # This manually set the progress bar to the middle according to the total_batch_idx.
         # asssume fix num batches in each epoch.
         self.main_progress_bar.update(trainer.total_batch_idx % trainer.num_training_batches)
 
-class KeyboardInterruptModelCheckpoint(pl.callbacks.ModelCheckpoint):
 
+class KeyboardInterruptModelCheckpoint(pl.callbacks.ModelCheckpoint):
     def on_keyboard_interrupt(self, trainer, pl_module):
         # Save model when keyboard interrupt
         filepath = os.path.join(self.dirpath, self.CHECKPOINT_NAME_LAST+'.ckpt')
         if not trainer.total_batch_idx % trainer.num_training_batches == 0:
             # end in the middle of a epoch
             trainer.current_epoch -= 1 # because when saving, it will be increased.
+        else:
+            # if the model is interrupted at the end of an epoch, for example during validation,
+            # the model will be resumed from the beginning of the next epoch.
+            pass
         self._save_model(trainer, filepath=filepath)
