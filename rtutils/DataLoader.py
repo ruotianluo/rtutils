7afc5a5feac724bfbc80045819f9d4bcb57455dd
ruotianluo <rluo@ttic.edu>
2021-06-05 15:22:02 -0500

    remove unnecessary imports

--- rtutils/DataLoader.py
+++ rtutils/DataLoader.py
@@ -1,8 +1,4 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
 import itertools
 import torch.distributed as dist
 
